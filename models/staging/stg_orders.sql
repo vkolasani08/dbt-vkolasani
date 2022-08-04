c0bc8bc2178e408b99acd6043ece94c7ee85662c
vkolasani08@gmail.com <vkolasani08@gmail.com>
2022-08-04 13:42:38 +0000

    stagibg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,24 @@
+select
+--from raw_orders
+orderid,
+orderdate,
+shipdate,
+shipmode,
+o.customerid,
+o.productid,
+o.ORDERSELLIINGPRICE,
+o.ordercostprice,
+--from raw_customer
+customername,
+segment,
+country,
+--from raw_product
+category,
+productname,
+subcategory,
+ORDERSELLIINGPRICE - ordercostprice as orderprofit
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p
+on o.productid = p.productid
